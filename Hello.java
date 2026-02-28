af70f450f4521dc12f06535d11122ca89b0f267d
Lakshmi Pillai <pillailakshmi099@gmail.com>
2026-03-01 01:48:48 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins GitHub Job!");
+        System.out.println("Hello from Poll SCM Trigger!");
     }
+
 }
