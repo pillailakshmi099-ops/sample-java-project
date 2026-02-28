7ef399b977b7003f99e18122ccb520da8ee27a34
Lakshmi Pillai <pillailakshmi099@gmail.com>
2026-03-01 02:52:36 +0530

    Update Hello.java for webhook trigger

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Poll SCM Trigger!");
+        System.out.println("Hello from Webhook Trigger!");
     }
 
 }
+
