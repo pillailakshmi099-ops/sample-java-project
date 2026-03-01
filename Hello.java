1db4bdcd58debb1d9d2d8fecce5309e2a726824d
Lakshmi Pillai <pillailakshmi099@gmail.com>
2026-03-01 14:01:53 +0530

    Update Hello.java for webhook

--- Hello.java
+++ Hello.java
@@ -1,8 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Webhook Trigger!!");
+        System.out.println("Hello this is MCA25085 performing Webhook Trigger!!");
     }
 
 }
+
 
 
